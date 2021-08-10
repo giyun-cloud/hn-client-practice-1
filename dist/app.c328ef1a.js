ced4fff0a35dc6502216c053b7ca850665d782d3
giyun-cloud <nangiyoon@gmail.com>
2021-08-10 12:37:22 +0900

    paging

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,6 +123,9 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+var store = {
+  currentPage: 1
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -135,24 +138,28 @@ function newsFeed() {
   var newsList = [];
   newsList.push("<ul>");
 
-  for (var i = 0; i < 10; i++) {
-    newsList.push("\n      <li>\n        <a href=\"#".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n      <li>\n        <a href=\"#/show/".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
   }
 
   newsList.push("</ul>");
+  newsList.push("\n    <div>\n      <a href='#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "'>\uC774\uC804 \uD398\uC774\uC9C0<a/>\n      <a href='#/page/").concat(store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : Math.ceil(newsFeed.length / 10), "'>\uB2E4\uC74C \uD398\uC774\uC9C0<a/>\n    </div>\n  "));
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  var id = location.hash.substr(1);
+  var id = location.hash.substr(7);
   var newsContent = getData(CONTENT_URL.replace("@id", id));
-  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href='#'>\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href='#/page/").concat(store.currentPage, "'>\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
 }
 
 function router() {
   var routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
